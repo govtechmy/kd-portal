cde155f02886e74a5cd08f2b3de7293b1ce90bfb
Len E-Herng <leneherng@gmail.com>
2024-07-04 21:22:28 +0800

    i18n: use solid lock icon

--- src/components/layout/masthead.tsx
+++ src/components/layout/masthead.tsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 import ChevronDown from "@/icons/chevron-down";
 import { cn } from "@/lib/utils";
 import Lock from "@/icons/lock";
+import SolidLock from "@/icons/solid-lock";
 
 export default function Masthead() {
   const t = useTranslations("Masthead");
@@ -14,55 +15,57 @@ export default function Masthead() {
 
   return (
     <div
-      className={cn(
-        "px-4.5 py-1 sm:px-6",
+      className={
         open
           ? "bg-gradient-to-b from-washed-100 from-[84.74%] to-outline-200 to-100%"
-          : "bg-washed-100",
-      )}
+          : "bg-washed-100"
+      }
     >
-      <div className="flex flex-wrap items-center gap-1.5 text-sm leading-4 text-brand-700">
-        <Checkmark14PointStar className="size-4 sm:size-5" />
-        <span className="text-black-700">{t("official_gov_website")}</span>
-        <button
-          className="flex items-center gap-0.5"
-          onClick={() => setOpen(!open)}
-        >
-          {t("how_to_identify")}
-          <ChevronDown
-            className={cn(
-              "size-4 transition duration-200",
-              open ? "rotate-180" : "",
-            )}
-          />
-        </button>
-      </div>
-      {open && (
-        <div className="grid grid-cols-1 gap-6 pb-8 pt-6 sm:grid-cols-2">
-          <div className="flex gap-3">
-            <GovMY className="text-foreground-success shrink-0" />
-            <div className="space-y-1.5">
-              <p className="font-medium">{t("official")}</p>
-              <p className="max-w-prose text-balance text-sm text-black-700">
-                {t("not_govmy")}
-                <span className="font-semibold">.gov.my</span>
-                {t("close_site")}
-              </p>
-            </div>
-          </div>
-          <div className="flex gap-3">
-            <Lock className="text-foreground-success shrink-0" />
-            <div className="space-y-1.5">
-              <p className="font-medium">{t("secure")}</p>
-              <p className="max-w-prose text-balance text-sm text-black-700">
-                {t("find_lock")}
-                <span className="font-semibold">https://</span>
-                {t("precaution")}
-              </p>
-            </div>
-          </div>
+      <div className="container">
+        <div className="flex flex-wrap items-center gap-1.5 py-1 text-sm leading-4 text-brand-700">
+          <Checkmark14PointStar className="size-4 sm:size-5" />
+          <span className="text-black-700">{t("official_gov_website")}</span>
+          <button
+            className="flex items-center gap-0.5"
+            onClick={() => setOpen(!open)}
+          >
+            {t("how_to_identify")}
+            <ChevronDown
+              className={cn(
+                "size-4 transition duration-200",
+                open ? "rotate-180" : "",
+              )}
+            />
+          </button>
         </div>
-      )}
+        {open && (
+          <div className="grid grid-cols-1 gap-6 pb-8 pt-6 sm:grid-cols-2">
+            <div className="flex gap-3">
+              <GovMY className="text-foreground-success shrink-0" />
+              <div className="space-y-1.5">
+                <p className="font-medium">{t("official")}</p>
+                <p className="max-w-prose text-balance text-sm text-black-700">
+                  {t("not_govmy")}
+                  <span className="font-semibold">.gov.my</span>
+                  {t("close_site")}
+                </p>
+              </div>
+            </div>
+            <div className="flex gap-3">
+              <Lock className="text-foreground-success shrink-0" />
+              <div className="space-y-1.5">
+                <p className="font-medium">{t("secure")}</p>
+                <div className="max-w-prose text-balance text-sm text-black-700">
+                  {t("find_lock")} <SolidLock className="inline size-4" />{" "}
+                  {t("or")}
+                  <span className="font-semibold">https://</span>
+                  {t("precaution")}
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
