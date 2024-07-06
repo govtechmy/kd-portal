13a3015172609ca1b7a50f1814b6db6b3f0f526e
Amaluddin Asyraf <amaluddinasyraf@gmail.com>
2024-07-07 00:29:29 +0800

    test build

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,4 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ['next/core-web-vitals'],
-  parserOptions: {
-    project: ['./tsconfig.json'],
-    tsconfigRootDir: __dirname,
-  },
-}
+  extends: ["custom/next"],
+};
