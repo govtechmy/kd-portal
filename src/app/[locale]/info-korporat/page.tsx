146a322d3fc9b3b465c869687a1027f95ed40ccb
Len E-Herng <leneherng@gmail.com>
2024-07-06 19:30:58 +0800

    feat: visi, misi, function role

--- src/app/[locale]/info-korporat/page.tsx
+++ src/app/[locale]/info-korporat/page.tsx
@@ -1,4 +1,12 @@
 import { buttonVariants } from "@/components/ui/button";
+import Bolt from "@/icons/bolt";
+import CheckmarkShield from "@/icons/checkmark-shield";
+import EyeShow from "@/icons/eye-show";
+import Flag from "@/icons/flag";
+import Globe from "@/icons/globe";
+import Gov from "@/icons/gov";
+import Money from "@/icons/money";
+import Trophy from "@/icons/trophy";
 import UserGroup from "@/icons/user-group";
 import { routes } from "@/lib/routes";
 import { cn } from "@/lib/utils";
@@ -20,98 +28,118 @@ export default function Page({
   const t = useTranslations();
 
   return (
-    <>
-      <section className="flex flex-col gap-16 px-4.5 py-[84px] md:px-6">
-        <div className="mx-auto flex flex-col gap-6 text-center">
-          <p className="text-sm font-semibold uppercase tracking-[0.2em] text-brand-600">
-            {t("Info.background")}
-          </p>
-          <div className="flex flex-col items-center gap-4.5 font-poppins text-hxs font-semibold sm:flex-row sm:text-hsm">
-            <Image
-              src="/jata-negara.png"
-              width={65}
-              height={50}
-              alt="Jata Negara"
-            />
-            {t("Agency.name")}
+    <main className="divide-y-washed-100 divide-y">
+      <section className="container flex flex-col gap-16 py-[84px] lg:border-x lg:border-x-washed-100">
+        <div className="space-y-16">
+          <div className="flex flex-col items-center gap-6">
+            <p className="text-sm font-semibold uppercase tracking-[0.2em] text-brand-600">
+              {t("Info.background")}
+            </p>
+            <div className="flex flex-col items-center gap-4.5 font-poppins text-hxs font-semibold sm:flex-row sm:text-hsm">
+              <Image
+                src="/jata-negara.png"
+                width={65}
+                height={50}
+                alt="Jata Negara"
+              />
+              {t("Agency.name")}
+            </div>
           </div>
-        </div>
-        <div className="gap-[42px] lg:grid lg:grid-cols-4">
-          <div className="col-span-2 col-start-2">
-            <div className="mx-auto flex max-w-prose flex-col gap-[42px]">
-              <div>
-                <p className="font-poppins text-[46px] leading-none text-brand-600">
-                  “
-                </p>
-                <div className="space-y-6">
-                  <p className="text-balance text-xl text-black-700">
-                    Penubuhan sebuah kementerian khas iaitu Kementerian Digital
-                    adalah untuk memberi penekanan kepada agenda pendigitalan.
-                    Tanpa kementerian yang khas ini, Malaysia tidak akan dapat
-                    bersaing pantas dengan negara-negara lain dalam bidang
-                    digital.
+
+          <div className="gap-[42px] lg:grid lg:grid-cols-4">
+            <div className="col-span-2 col-start-2">
+              <div className="mx-auto flex max-w-prose flex-col gap-[42px]">
+                <div>
+                  <p className="font-poppins text-[46px] leading-none text-brand-600">
+                    “
                   </p>
-                  <p className="text-dim-500">
-                    –– YAB PM, Dato’ Seri Anwar Ibrahim
-                  </p>
+                  <div className="space-y-6">
+                    <p className="text-balance text-xl text-black-700">
+                      {t("Info.quote")}
+                    </p>
+                    <p className="text-dim-500">{t("Info.author")}</p>
+                  </div>
                 </div>
-              </div>
 
-              <div className="space-y-4.5">
-                <p>
-                  Semasa mengumumkan rombakan kabinet pada 12 Disember 2023, 
-                  YAB Perdana Menteri, Dato’ Seri Anwar Ibrahim telah
-                  mencetuskan kewujudan sebuah kementerian yang baru iaitu
-                  Kementerian Digital.
-                </p>
-                <p>
-                  Agenda digital negara yang disusun semula membolehkan
-                  pelaksanaannya yang lebih berfokus, holistik dan berkesan.
-                  Fungsi pendigitalan dalam konteks sektor awam sebelum ini yang
-                  diterajui oleh Unit Pemodenan Tadbiran dan Perancangan
-                  Pengurusan (MAMPU) turut diperkasakan melalui penstrukturan
-                  semula MAMPU yang kini dikenali sebagai Jabatan Digital Negara
-                  (JDN).
-                </p>
-                <p>
-                  Selain JDN, pasak kementerian ini dikuatkan lagi dengan
-                  kehadiran agensi-agensi yang telah lama bertapak dalam arena
-                  digitalisasi seperti  Jabatan Perlindungan Data Peribadi
-                  (JPDP), MyDIGITAL Corporation, Malaysia Digital Economy
-                  Corporation (MDEC), CyberSecurity Malaysia (CSM), Digital
-                  Nasional Berhad (DNB) dan MyNIC.
-                </p>
-              </div>
+                <p className="whitespace-pre-line">{t("Info.desc1")}</p>
+                <p className="whitespace-pre-line">{t("Info.desc2")}</p>
 
-              <div className="space-y-4.5">
-                <p>
-                  YB Tuan Gobind Singh Deo telah dilantik sebagai Menteri
-                  Digital yang pertama di Malaysia. Beliau dibantu oleh Timbalan
-                  Menteri, YB Datuk Wilson Ugak Anak Kumbong. Dari aspek
-                  pentadbiran awamnya pula, Y.Bhg Datuk Haji Rodzi Md Saad
-                  merupakan Ketua Setiausaha Kementerian Digital yang pertama.
-                </p>
-                <p>
-                  Ibu pejabat Kementerian Digital secara rasminya beroperasi di
-                  Menara Usahawan, Presint 2, Pusat Pentadbiran Kerajaan
-                  Persekutuan, Putrajaya.
-                </p>
+                <Link
+                  href={routes.DIRECTORY}
+                  className={cn(
+                    buttonVariants({ variant: "secondary", size: "md" }),
+                    "mt-3 w-fit rounded-full",
+                  )}
+                >
+                  <UserGroup />
+                  Direktori Kakitangan
+                </Link>
               </div>
-
-              <Link
-                href={routes.DIRECTORY}
-                className={cn(
-                  buttonVariants({ variant: "secondary", size: "md" }),
-                  "mt-3 w-fit rounded-full",
-                )}
-              >
-                <UserGroup />
-                Direktori Kakitangan
-              </Link>
             </div>
           </div>
         </div>
       </section>
-    </>
+
+      <section className="container grid grid-cols-1 divide-y divide-washed-100 md:grid-cols-2 md:divide-x lg:border-x lg:border-x-washed-100">
+        {[
+          {
+            icon: <EyeShow className="size-8" />,
+            title: "Info.visi",
+            desc: "Info.visi_desc",
+          },
+          {
+            icon: <Flag className="size-8" />,
+            title: "Info.misi",
+            desc: "Info.misi_desc",
+          },
+        ].map(({ icon, title, desc }) => (
+          <div className="flex gap-6 p-12 md:gap-8 md:px-16 md:py-[84px]">
+            <div className="size-[54px] rounded-full bg-brand-50 p-[11px] text-brand-700">
+              {icon}
+            </div>
+            <div className="space-y-3 lg:space-y-4.5">
+              <p className="text-hsm font-semibold text-brand-600">
+                {t(title)}
+              </p>
+              <p className="text-black-700">{t(desc)}</p>
+            </div>
+          </div>
+        ))}
+      </section>
+
+      <section className="container p-12 md:py-[84px] lg:border-x lg:border-washed-100 xl:grid xl:grid-cols-12">
+        <div className="col-span-10 col-start-2 space-y-12 md:space-y-[74px]">
+          <h2 className="text-center font-poppins text-hsm font-semibold">
+            {t("Info.Role.title")}
+          </h2>
+          <div className="grid grid-cols-1 gap-y-8 sm:gap-x-12 sm:gap-y-[72px] md:grid-cols-2 lg:grid-cols-3">
+            {[
+              <Gov className="size-8" />,
+              <Globe className="size-8" />,
+              <Bolt className="size-8" />,
+              <CheckmarkShield className="size-8" />,
+              <Money className="size-8" />,
+              <Trophy className="size-8" />,
+            ].map((icon, i) => (
+              <div className="flex flex-row gap-4.5 md:flex-col">
+                <div className="size-[54px] rounded-xl bg-brand-50 p-[11px] text-brand-700">
+                  {icon}
+                </div>
+
+                <p className="text-black-700">
+                  {t.rich(`Info.Role.desc${i + 1}`, {
+                    b: (chunks) => (
+                      <span className="font-semibold text-foreground">
+                        {chunks}
+                      </span>
+                    ),
+                  })}
+                </p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
