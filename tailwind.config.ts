3f2e98b6faae30ebb197bf7207390b0956394129
Len E-Herng <leneherng@gmail.com>
2024-07-06 19:15:31 +0800

    fix: remove max width if smaller than 1280px

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,10 +9,6 @@ const config: Config = {
     container: {
       center: true,
       screens: {
-        DEFAULT: "100%",
-        sm: "640px",
-        md: "768px",
-        lg: "1024px",
         xl: "1280px",
       },
       padding: {
