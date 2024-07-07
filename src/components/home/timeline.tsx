c492a63d6a736a53ba329a0ea5d4a89443d6ef3d
Len E-Herng <leneherng@gmail.com>
2024-07-07 18:19:18 +0800

    fix: timeline layout

--- src/components/home/timeline.tsx
+++ src/components/home/timeline.tsx
@@ -26,20 +26,30 @@ export default function Timeline() {
         <div className="relative flex h-full justify-start sm:justify-center lg:w-2/3">
           <div className="absolute -z-10 h-full w-px bg-outline-200 max-sm:left-[3.5px]" />
           <div className="grid grid-cols-1 gap-x-14 gap-y-3 py-8 max-sm:ml-8 sm:grid-cols-2 lg:py-[84px]">
-            {Array(7)
-              .fill(null)
-              .map((_, i) => (
+            {Array.from({ length: 7 }).map((_, i) => (
+              <div
+                key={i}
+                className="relative flex flex-col justify-center even:sm:translate-y-[calc(50%+6px)]"
+              >
                 <div
-                  key={i}
                   className={cn(
-                    "relative flex h-[136px] flex-col gap-1 rounded-xl border border-outline-200 px-4 py-3",
-                    "group hover:border-[1.5px] hover:border-brand-300 even:sm:translate-y-[76px]",
+                    "relative flex h-fit flex-col gap-1 rounded-xl border border-outline-200 px-4 py-3",
+                    "group hover:border-brand-300 hover:ring-1 hover:ring-brand-300",
                   )}
                 >
+                  <div
+                    className={cn(
+                      [3, 5].includes(i)
+                        ? "absolute right-3 top-2 flex size-6 items-center justify-center rounded-full bg-[#FFF1E5]"
+                        : "hidden",
+                    )}
+                  >
+                    <Star className="size-4 stroke-2 text-[#EA740F]" />
+                  </div>
                   <p className="line-clamp-1 text-xs font-medium uppercase tracking-widest text-dim-500">
                     {t(`date${i + 1}`)}
                   </p>
-                  <p className="line-clamp-1 font-medium text-black-900">
+                  <p className="font-medium text-black-900">
                     {t(`title${i + 1}`)}
                   </p>
                   <p className="line-clamp-3 text-sm text-black-700">
@@ -58,22 +68,15 @@ export default function Timeline() {
                           ? "group-hover:-right-[6.5px] max-sm:-left-1.5 group-hover:max-sm:-left-[6.5px] sm:-right-1.5"
                           : "-left-1.5 group-hover:-left-[6.5px]",
                         "absolute top-1/2 size-2 -translate-y-1/2 transform rounded-full bg-brand-600",
-                        // "ring-brand-300 ring-offset-[3px] group-hover:ring",
+                        "ring-brand-300 ring-offset-[3px] group-hover:ring",
                       )}
                     >
-                      <div
-                        className={cn(
-                          // i % 2 === 0
-                          //   ? "max-sm:right-full sm:left-full"
-                          //   : "right-full",
-                          "left-[3px] top-2",
-                          "absolute h-[60px] w-0.5 bg-gradient-to-b from-brand-600 from-0% to-transparent to-100%",
-                        )}
-                      />
+                      <div className="absolute left-[3px] top-2 h-[60px] w-0.5 bg-gradient-to-b from-brand-600 from-0% to-transparent to-100%" />
                     </div>
                   </div>
                 </div>
-              ))}
+              </div>
+            ))}
           </div>
         </div>
       </div>
