02986ff650f261ee5e84c4062bc9719fdf86c56f
Len E-Herng <leneherng@gmail.com>
2024-07-11 21:11:31 +0800

    feat: sitemap

--- env.d.ts
+++ env.d.ts
@@ -1,6 +1,7 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
     APP_ENV: string;
+    APP_URL: string;
     REVALIDATE_TOKEN: string;
     AUTH_TOKEN: string;
     GOOGLE_API_KEY: string;
