faf52a4b3678a6acc1e090b8ed88cb2745e4f7ae
Len E-Herng <leneherng@gmail.com>
2024-07-18 20:48:13 +0800

    feat: escape html

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,20 +5,57 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const toRM = (value: number | null): string => {
-  if (value === null) return "-";
-  return new Intl.NumberFormat("ms-MY", {
-    style: "currency",
-    currency: "MYR",
-    maximumFractionDigits: 0,
-    minimumFractionDigits: 0,
-  }).format(value);
-};
+const matchHtmlRegExp = /["'&<>]/;
 
-export const toComma = (value: number | null): string => {
-  if (value === null) return "-";
-  return new Intl.NumberFormat("ms-MY", {
-    maximumFractionDigits: 0,
-    minimumFractionDigits: 0,
-  }).format(value);
-};
+/**
+ * Escape special characters in the given string of html.
+ *
+ * @param  {string} string The string to escape for inserting into HTML
+ * @return {string}
+ */
+export function escapeHTML(string: string) {
+  var str = '' + string;
+  var match = matchHtmlRegExp.exec(str);
+
+  if (!match) {
+    return str;
+  }
+
+  var escape;
+  var html = '';
+  var index = 0;
+  var lastIndex = 0;
+
+  for (index = match.index; index < str.length; index++) {
+    switch (str.charCodeAt(index)) {
+      case 34: // "
+        escape = '&quot;';
+        break;
+      case 38: // &
+        escape = '&amp;';
+        break;
+      case 39: // '
+        escape = '&#39;';
+        break;
+      case 60: // <
+        escape = '&lt;';
+        break;
+      case 62: // >
+        escape = '&gt;';
+        break;
+      default:
+        continue;
+    }
+
+    if (lastIndex !== index) {
+      html += str.substring(lastIndex, index);
+    }
+
+    lastIndex = index + 1;
+    html += escape;
+  }
+
+  return lastIndex !== index
+    ? html + str.substring(lastIndex, index)
+    : html;
+}
