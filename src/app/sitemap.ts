827cc7cbefc5afbbd784ab2d45ba47ea2253b2de
Len E-Herng <leneherng@gmail.com>
2024-07-19 13:50:12 +0800

    test: sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,68 +1,20 @@
+import { locales, defaultLocale } from "@/lib/i18n-config";
 import { routes } from "@/lib/routes";
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const { APP_URL } = process.env;
-  return [
-    {
-      url: `${APP_URL}`,
-      lastModified: new Date(),
-      alternates: {
-        languages: {
-          "en-GB": `${APP_URL}/en-GB`,
-          "ms-MY": `${APP_URL}`,
-        },
-      },
+  const host = process.env.APP_URL;
+
+  function getUrl(key: string, locale: (typeof locales)[number]) {
+    return `${host}/${locale}${key === "/" ? "" : key}`;
+  }
+
+  return Object.values(routes).map((path) => ({
+    url: getUrl(path, defaultLocale),
+    alternates: {
+      languages: Object.fromEntries(
+        locales.map((locale) => [locale, getUrl(path, locale)]),
+      ),
     },
-    {
-      url: `${APP_URL}${routes.DISCLAIMER}`,
-      lastModified: new Date(),
-      alternates: {
-        languages: {
-          "en-GB": `${APP_URL}/en-GB${routes.DISCLAIMER}`,
-          "ms-MY": `${APP_URL}${routes.DISCLAIMER}`,
-        },
-      },
-    },
-    {
-      url: `${APP_URL}${routes.PRIVACY}`,
-      lastModified: new Date(),
-      alternates: {
-        languages: {
-          "en-GB": `${APP_URL}/en-GB${routes.PRIVACY}`,
-          "ms-MY": `${APP_URL}${routes.PRIVACY}`,
-        },
-      },
-    },
-    {
-      url: `${APP_URL}${routes.MINISTRY_PROFILE}`,
-      lastModified: new Date(),
-      alternates: {
-        languages: {
-          "en-GB": `${APP_URL}/en-GB${routes.MINISTRY_PROFILE}`,
-          "ms-MY": `${APP_URL}${routes.MINISTRY_PROFILE}`,
-        },
-      },
-    },
-    {
-      url: `${APP_URL}${routes.DIRECTORY}`,
-      lastModified: new Date(),
-      alternates: {
-        languages: {
-          "en-GB": `${APP_URL}/en-GB${routes.DIRECTORY}`,
-          "ms-MY": `${APP_URL}${routes.DIRECTORY}`,
-        },
-      },
-    },
-    {
-      url: `${APP_URL}${routes.CONTACT_US}`,
-      lastModified: new Date(),
-      alternates: {
-        languages: {
-          "en-GB": `${APP_URL}/en-GB${routes.CONTACT_US}`,
-          "ms-MY": `${APP_URL}${routes.CONTACT_US}`,
-        },
-      },
-    },
-  ];
+  }));
 }
