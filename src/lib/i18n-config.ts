c55a4c957137a785fb6ae63f03334821e1cc1915
Amaluddin Asyraf <amaluddinasyraf@gmail.com>
2024-07-21 22:15:43 +0800

    fix

--- src/lib/i18n-config.ts
+++ src/lib/i18n-config.ts
@@ -1,5 +1,5 @@
 import { LocalePrefix } from "next-intl/routing";
 
 export const defaultLocale = "ms-MY";
-export const locales = [defaultLocale, "en-GB"] as const;
+export const locales = [defaultLocale, "en-GB"];
 export const localePrefix = "as-needed" satisfies LocalePrefix;
