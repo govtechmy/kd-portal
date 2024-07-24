4160aecebbaf4b7abca9c01586843a642cf42a6f
Amaluddin Asyraf <amaluddinasyraf@gmail.com>
2024-07-24 22:24:15 +0800

    fix carousel

--- src/components/home/carousel.tsx
+++ src/components/home/carousel.tsx
@@ -47,77 +47,84 @@ export default function ActivityCarousel({
   return (
     <Section>
       <div className="grid-cols-12 gap-6 border-washed-100 px-4.5 py-12 lg:px-0 lg:py-[84px] xl:grid xl:border-x">
-        <Carousel setApi={setApi} className="col-span-10 col-start-2 space-y-6">
-          <div className="flex flex-col-reverse gap-8 lg:flex-row lg:gap-12">
-            <div className="space-y-3 sm:space-y-4.5 lg:w-1/3">
-              <Overline>{t("overline")}</Overline>
-              <h2 className="text-balance font-poppins text-2xl font-semibold lg:text-hsm">
-                {typeof items[current].achievements !== "string" &&
-                  items[current].achievements.title}
-              </h2>
-              <p className="text-pretty text-black-700">
-                {typeof items[current].achievements !== "string" &&
-                  items[current].achievements.description}
-              </p>
+        {Boolean(items.length) && (
+          <Carousel
+            setApi={setApi}
+            className="col-span-10 col-start-2 space-y-6"
+          >
+            <div className="flex flex-col-reverse gap-8 lg:flex-row lg:gap-12">
+              <div className="space-y-3 sm:space-y-4.5 lg:w-1/3">
+                <Overline>{t("overline")}</Overline>
+                <h2 className="text-balance font-poppins text-2xl font-semibold lg:text-hsm">
+                  {typeof items[current].achievements !== "string" &&
+                    items[current].achievements.title}
+                </h2>
+                <p className="text-pretty text-black-700">
+                  {typeof items[current].achievements !== "string" &&
+                    items[current].achievements.description}
+                </p>
+              </div>
+
+              <CarouselContent className="rounded-[32px] border border-outline-200 bg-outline-200 shadow-card sm:shadow-[0_30px_100px_-10px_#4C53614D] lg:w-2/3">
+                {items.map((item, index) => {
+                  const media =
+                    typeof item.achievements !== "string"
+                      ? typeof item.achievements.achievement_file !== "string"
+                        ? item.achievements.achievement_file
+                        : ""
+                      : "";
+                  return (
+                    <CarouselItem key={index}>
+                      <Image
+                        priority
+                        src={media && media.url ? media.url : ""}
+                        width={750}
+                        height={450}
+                        alt={
+                          media && media.url ? media.caption || media.alt : ""
+                        }
+                        className="h-[300px] w-full select-none object-cover sm:h-[450px]"
+                      />
+                    </CarouselItem>
+                  );
+                })}
+              </CarouselContent>
             </div>
 
-            <CarouselContent className="rounded-[32px] border border-outline-200 bg-outline-200 shadow-card sm:shadow-[0_30px_100px_-10px_#4C53614D] lg:w-2/3">
-              {items.map((item, index) => {
-                const media =
-                  typeof item.achievements !== "string"
-                    ? typeof item.achievements.achievement_file !== "string"
-                      ? item.achievements.achievement_file
-                      : ""
-                    : "";
-                return (
-                  <CarouselItem key={index}>
-                    <Image
-                      priority
-                      src={media && media.url ? media.url : ""}
-                      width={750}
-                      height={450}
-                      alt={media && media.url ? media.caption || media.alt : ""}
-                      className="h-[300px] w-full select-none object-cover sm:h-[450px]"
-                    />
-                  </CarouselItem>
-                );
-              })}
-            </CarouselContent>
-          </div>
+            <div className="flex flex-col-reverse items-center gap-6 lg:flex-row lg:justify-between">
+              <div className="space-x-3">
+                <CarouselPrevious className="static size-[42px] transform-none" />
+                <CarouselNext className="static size-[42px] transform-none" />
+              </div>
 
-          <div className="flex flex-col-reverse items-center gap-6 lg:flex-row lg:justify-between">
-            <div className="space-x-3">
-              <CarouselPrevious className="static size-[42px] transform-none" />
-              <CarouselNext className="static size-[42px] transform-none" />
+              <div className="flex items-center gap-x-2">
+                {Array(images.length)
+                  .fill(null)
+                  .map((_, i) => (
+                    <svg
+                      key={i}
+                      viewBox={i === current ? "0 0 15 8" : "0 0 8 8"}
+                      className={cn(
+                        "transition-[colors,_width] duration-300 ease-in-out",
+                        i === current
+                          ? "h-2 w-[15px] text-foreground-primary"
+                          : "size-2 text-outline-300",
+                      )}
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <rect
+                        x="0"
+                        width={i === current ? 15 : 8}
+                        height="8"
+                        rx="4"
+                        fill="currentColor"
+                      />
+                    </svg>
+                  ))}
+              </div>
             </div>
-
-            <div className="flex items-center gap-x-2">
-              {Array(images.length)
-                .fill(null)
-                .map((_, i) => (
-                  <svg
-                    key={i}
-                    viewBox={i === current ? "0 0 15 8" : "0 0 8 8"}
-                    className={cn(
-                      "transition-[colors,_width] duration-300 ease-in-out",
-                      i === current
-                        ? "h-2 w-[15px] text-foreground-primary"
-                        : "size-2 text-outline-300",
-                    )}
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <rect
-                      x="0"
-                      width={i === current ? 15 : 8}
-                      height="8"
-                      rx="4"
-                      fill="currentColor"
-                    />
-                  </svg>
-                ))}
-            </div>
-          </div>
-        </Carousel>
+          </Carousel>
+        )}
       </div>
     </Section>
   );
