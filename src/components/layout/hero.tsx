5d53963ef5b6ba45993bb58c5e9116ed81067e19
PaanJoe <38974707+paanjoe@users.noreply.github.com>
2025-07-25 17:22:06 +0800

    chore: splask privacy tagging

--- src/components/layout/hero.tsx
+++ src/components/layout/hero.tsx
@@ -14,7 +14,7 @@ export default function Hero({
       </div>
 
       <div className="container flex flex-col gap-6 py-16">
-        <h1 className="text-center font-poppins text-[2rem]/10 sm:text-hmd font-semibold">
+        <h1 splwpk-privacy-policy="splwpk-privacy-policy" className="text-center font-poppins text-[2rem]/10 sm:text-hmd font-semibold">
           {title}
         </h1>
         {search}
